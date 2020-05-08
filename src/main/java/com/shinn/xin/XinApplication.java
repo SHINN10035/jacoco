c49bb89b7fba4bc710058aa881261b1eefc33fcd
SHINN10035 <2593327338@qq.com>
2020-05-08 15:25:57 +0800

    1

---
+++ src/main/java/com/shinn/xin/XinApplication.java
@@ -0,0 +1,13 @@
+package com.shinn.xin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shinn/xin/XinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinn.xin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
